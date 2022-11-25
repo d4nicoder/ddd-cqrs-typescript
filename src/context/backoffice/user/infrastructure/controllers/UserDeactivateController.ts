1f1a8e3e97b0a124b5bfb3fd77c339ad79277c52
@d4nicoder <d4nicoder@gmail.com>
2022-11-26 00:37:46 +0100

    test(user): Added controller to deactivate users

---
+++ src/context/backoffice/user/infrastructure/controllers/UserDeactivateController.ts
@@ -0,0 +1,26 @@
+import { registerController } from '../../../../../shared/container/registerController'
+import { Controller, ControllerDefinition } from '../../../../../shared/domain/Controller'
+import { ControllerRequest } from '../../../../../shared/domain/ControllerRequest'
+import { ControllerResponse } from '../../../../../shared/domain/ControllerResponse'
+import { UserDeactivateService } from '../../application/UserDeactivateService'
+
+@registerController()
+export class UserDeactivateController extends Controller {
+  constructor (private userDeactivateService: UserDeactivateService) {
+    super()
+  }
+  getDefinition (): ControllerDefinition {
+    return {
+      method: 'PATCH',
+      onlyAdmin: true,
+      path: '/api/v1/users/:id/deactivate',
+      requiredAuth: true
+    }
+  }
+
+  async run (req: ControllerRequest): Promise<ControllerResponse> {
+    const id = req.params.id
+    await this.userDeactivateService.run(id)
+    return new ControllerResponse().setStatus(204)
+  }
+}
